3a1e18610b2fc71ad19d2788b5b0ab71f3aa1778
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-08-27 22:07:22 +0900

    added module for open stmt

---
+++ src/enum/open.f90
@@ -0,0 +1,18 @@
+module enumul_open
+    use :: enumul_open_access, &
+        only:enum_open_access, open_access, get_open_access_default_expr, optval
+    implicit none
+    private
+
+    ! enum types
+    public :: enum_open_access
+
+    ! enum lists
+    public :: open_access
+
+    ! default values
+    public :: get_open_access_default_expr
+
+    ! optval procedures
+    public :: optval
+end module enumul_open
