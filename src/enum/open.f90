bdc550f26e08668261fdb93a1554d6db2034486b
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-08-28 11:27:03 +0900

    added enumerator for pad spec in open stmt

--- src/enum/open.f90
+++ src/enum/open.f90
@@ -15,6 +15,8 @@ module enumul_open
         only:enum_open_encoding, open_encoding, default_open_encoding, get_open_encoding_default, optval
     use :: enumul_open_form, &
         only:enum_open_form, open_form, default_open_form, get_open_form_default, optval
+    use :: enumul_open_pad, &
+        only:enum_open_pad, open_pad, default_open_pad, get_open_pad_default, optval
     use :: enumul_open_status, &
         only:enum_open_status, open_status, default_open_status, get_open_status_default, optval
     implicit none
@@ -29,6 +31,7 @@ module enumul_open
     public :: enum_open_delim
     public :: enum_open_encoding
     public :: enum_open_form
+    public :: enum_open_pad
     public :: enum_open_status
 
     ! enum lists
@@ -40,6 +43,7 @@ module enumul_open
     public :: open_delim
     public :: open_encoding
     public :: open_form
+    public :: open_pad
     public :: open_status
 
     ! default values
@@ -51,6 +55,7 @@ module enumul_open
     public :: get_open_delim_default, default_open_delim
     public :: get_open_encoding_default, default_open_encoding
     public :: get_open_form_default, default_open_form
+    public :: get_open_pad_default, default_open_pad
     public :: get_open_status_default, default_open_status
 
     ! optval procedures
---
+++ src/enum/open/pad.f90
@@ -0,0 +1,91 @@
+module enumul_open_pad
+    use, intrinsic :: iso_c_binding
+    use :: enumul, only:enum_atype
+    implicit none
+    private
+    public :: optval
+    public :: get_open_pad_default
+
+    enum, bind(c)
+        enumerator :: Open_Pad_Undefined = 0
+        enumerator :: Open_Pad_Yes
+        enumerator :: Open_Pad_No
+    end enum
+
+    character(*), parameter, private :: pad(0:*) = ["UNDEFINED", &
+                                                    "YES      ", &
+                                                    "NO       "]
+        !! The possible character expressions
+        !! for the `pad` specifier in the `open` statement.
+        !!
+        !!@note
+        !! `"UNDEFINED"` is not the possible character-expression
+        !! but the `inquire` statment assigns `"UNDEFINED"`
+        !! to the specified variable if there is no connection.
+        !!@endnote
+
+    !>The enumerator for the `pad` specifier
+    !>and expression in the `open` statement.
+    type, public, extends(enum_atype) :: enum_open_pad
+        character(len=len(pad)), public :: expr
+            !! An expression for the `pad` specifier.
+            !! Open to public to pass the `open` statement.
+    end type enum_open_pad
+
+    type :: enum_open_pad_list
+        type(enum_open_pad), public :: undefined
+            !! The enumerator to represent the `UNDEFINED`
+        type(enum_open_pad), public :: yes
+            !! The enumerator to represent the `YES`
+        type(enum_open_pad), public :: no
+            !! The enumerator to represent the `NO`
+    end type enum_open_pad_list
+
+    type(enum_open_pad_list), public, parameter :: &
+        open_pad = &
+            enum_open_pad_list( &
+                undefined = enum_open_pad(Open_Pad_Undefined, pad(Open_Pad_Undefined)), &
+                yes       = enum_open_pad(Open_Pad_Yes      , pad(Open_Pad_Yes)), &
+                no        = enum_open_pad(Open_Pad_No       , pad(Open_Pad_No)) &
+            ) !&
+        !! The enumerators of possible expressions
+        !! for the `pad` specifier.
+
+    type(enum_open_pad), public, parameter :: &
+        default_open_pad = open_pad%yes
+        !! The default value of the `pad` specifier.
+        !! It is `"YES"`.
+
+    interface optval
+        procedure :: optval_open_pad
+    end interface
+contains
+    !>Returns `x` if it is presented, and
+    !>returns `default` if the `x` is not presented.
+    function optval_open_pad(x, default) result(y)
+        implicit none
+        type(enum_open_pad), intent(in), optional :: x
+        type(enum_open_pad), intent(in) :: default
+
+        type(enum_open_pad) :: y
+
+        if (present(x)) then
+            y = x
+        else
+            y = default
+        end if
+    end function optval_open_pad
+
+    !>Returns the enumerator representing the default
+    !>character-expression for the `pad` specifier
+    !>in the `open` statement.
+    !>The default value is `"YES"`.
+    function get_open_pad_default() result(default)
+        implicit none
+        type(enum_open_pad) :: default
+            !! The enumerator for default value of `pad` specifier
+            !! in `open`
+
+        default = default_open_pad
+    end function get_open_pad_default
+end module enumul_open_pad
