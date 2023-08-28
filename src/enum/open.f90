3995b15af70b6631646e8205812701021b9337ce
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-08-28 10:31:29 +0900

    added enumerator for blank spec in open stmt

--- src/enum/open.f90
+++ src/enum/open.f90
@@ -5,6 +5,8 @@ module enumul_open
         only:enum_open_action, open_action, get_open_action_default, optval
     use :: enumul_open_asynchronous, &
         only:enum_open_asynchronous, open_asynchronous, default_open_asynchronous, get_open_asynchronous_default, optval
+    use :: enumul_open_blank, &
+        only:enum_open_blank, open_blank, default_open_blank, get_open_blank_default, optval
     use :: enumul_open_status, &
         only:enum_open_status, open_status, default_open_status, get_open_status_default, optval
     implicit none
@@ -14,18 +16,21 @@ module enumul_open
     public :: enum_open_access
     public :: enum_open_action
     public :: enum_open_asynchronous
+    public :: enum_open_blank
     public :: enum_open_status
 
     ! enum lists
     public :: open_access
     public :: open_action
     public :: open_asynchronous
+    public :: open_blank
     public :: open_status
 
     ! default values
     public :: get_open_access_default, default_open_access
     public :: get_open_action_default
     public :: get_open_asynchronous_default, default_open_asynchronous
+    public :: get_open_blank_default, default_open_blank
     public :: get_open_status_default, default_open_status
 
     ! optval procedures
---
+++ src/enum/open/blank.f90
@@ -0,0 +1,92 @@
+module enumul_open_blank
+    use, intrinsic :: iso_c_binding
+    use :: enumul, only:enum_atype
+    implicit none
+    private
+    public :: optval
+    public :: get_open_blank_default
+
+    enum, bind(c)
+        enumerator :: Open_Blank_Undefined = 0
+        enumerator :: Open_Blank_Null
+        enumerator :: Open_Blank_Zero
+    end enum
+
+    character(*), parameter, private :: blank(0:*) = ["UNDEFINED ", &
+                                                      "NULL      ", &
+                                                      "ZERO      "]
+        !! The possible character expressions
+        !! for the `blank` specifier in the `open` statement.
+        !!
+        !!@note
+        !! `"UNDEFINED"` is not the possible character-expression
+        !! but the `inquire` statment assigns `"UNDEFINED"`
+        !! to the specified variable if there is no connection.
+        !!@endnote
+
+    !>The enumerator for the `blank` specifier
+    !>and expression in the `open` statement.
+    type, public, extends(enum_atype) :: enum_open_blank
+        character(len=len(blank)), public :: expr
+            !! An expression for the `blank` specifier.
+            !! Open to public to pass the `open` statement.
+    end type enum_open_blank
+
+    !>The possible expressions for the `blank` specifier
+    type :: enum_open_blank_list
+        type(enum_open_blank), public :: undefined
+            !! The enumerator to represent the `UNDEFINED`
+        type(enum_open_blank), public :: null
+            !! The enumerator to represent the `NULL`
+        type(enum_open_blank), public :: zero
+            !! The enumerator to represent the `ZEOR`
+    end type enum_open_blank_list
+
+    type(enum_open_blank_list), public, parameter :: &
+        open_blank = &
+            enum_open_blank_list( &
+                undefined = enum_open_blank(Open_Blank_Undefined, blank(Open_Blank_Undefined)), &
+                null      = enum_open_blank(Open_Blank_Null     , blank(Open_Blank_Null)), &
+                zero      = enum_open_blank(Open_Blank_Zero     , blank(Open_Blank_Zero)) &
+            ) !&
+        !! The enumerators of possible expressions
+        !! for the `blank` specifier.
+
+    type(enum_open_blank), public, parameter :: &
+        default_open_blank = open_blank%null
+        !! The default value of the `blank` specifier.
+        !! It is `"NULL"`.
+
+    interface optval
+        procedure :: optval_open_blank
+    end interface
+contains
+    !>Returns `x` if it is presented, and
+    !>returns `default` if the `x` is not presented.
+    function optval_open_blank(x, default) result(y)
+        implicit none
+        type(enum_open_blank), intent(in), optional :: x
+        type(enum_open_blank), intent(in) :: default
+
+        type(enum_open_blank) :: y
+
+        if (present(x)) then
+            y = x
+        else
+            y = default
+        end if
+    end function optval_open_blank
+
+    !>Returns the enumerator representing the default
+    !>character-expression for the `blank` specifier
+    !>in the `open` statement.
+    !>The default value is `"NULL"`.
+    function get_open_blank_default() result(default)
+        implicit none
+        type(enum_open_blank) :: default
+            !! The enumerator for default value of `blank` specifier
+            !! in `open`
+
+        default = default_open_blank
+    end function get_open_blank_default
+end module enumul_open_blank
