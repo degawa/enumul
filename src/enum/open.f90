6d03c9947b9a182b898d13db2f7048a1586a2f49
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-08-28 12:04:40 +0900

    added enumerator for round spce in open stmt

--- src/enum/open.f90
+++ src/enum/open.f90
@@ -19,6 +19,8 @@ module enumul_open
         only:enum_open_pad, open_pad, default_open_pad, get_open_pad_default, optval
     use :: enumul_open_position, &
         only:enum_open_position, open_position, default_open_position, get_open_position_default, optval
+    use :: enumul_open_round, &
+        only:enum_open_round, open_round, get_open_round_default, optval
     use :: enumul_open_status, &
         only:enum_open_status, open_status, default_open_status, get_open_status_default, optval
     implicit none
@@ -35,6 +37,7 @@ module enumul_open
     public :: enum_open_form
     public :: enum_open_pad
     public :: enum_open_position
+    public :: enum_open_round
     public :: enum_open_status
 
     ! enum lists
@@ -48,6 +51,7 @@ module enumul_open
     public :: open_form
     public :: open_pad
     public :: open_position
+    public :: open_round
     public :: open_status
 
     ! default values
@@ -61,6 +65,7 @@ module enumul_open
     public :: get_open_form_default, default_open_form
     public :: get_open_pad_default, default_open_pad
     public :: get_open_position_default, default_open_position
+    public :: get_open_round_default
     public :: get_open_status_default, default_open_status
 
     ! optval procedures
---
+++ src/enum/open/round.f90
@@ -0,0 +1,140 @@
+module enumul_open_round
+    use, intrinsic :: iso_c_binding
+    use :: enumul, only:enum_atype
+    implicit none
+    private
+    public :: optval
+    public :: get_open_round_default
+
+    enum, bind(c)
+        enumerator :: Open_Round_Undefined = 0
+        enumerator :: Open_Round_Up
+        enumerator :: Open_Round_Down
+        enumerator :: Open_Round_Zero
+        enumerator :: Open_Round_Nearest
+        enumerator :: Open_Round_Compatible
+        enumerator :: Open_Round_ProcessorDefined
+    end enum
+
+    character(*), parameter, private :: round(0:*) = ["UNDEFINED        ", &
+                                                      "UP               ", &
+                                                      "DOWN             ", &
+                                                      "ZERO             ", &
+                                                      "NEAREST          ", &
+                                                      "COMPATIBLE       ", &
+                                                      "PROCESSOR_DEFINED"]
+        !! The possible character expressions
+        !! for the `round` specifier in the `open` statement.
+        !!
+        !!@note
+        !! `"UNDEFINED"` is not the possible character-expression
+        !! but the `inquire` statment assigns `"UNDEFINED"`
+        !! to the specified variable if there is no connection or
+        !! if the connection is not for formatted I/O.
+        !!@endnote
+
+    !>The enumerator for the `round` specifier
+    !>and expression in the `open` statement.
+    type, public, extends(enum_atype) :: enum_open_round
+        character(len=len(round)), public :: expr
+            !! An expression for the `round` specifier.
+            !! Open to public to pass the `open` statement.
+    end type enum_open_round
+
+    !>The possible expressions for the `round` specifier
+    type :: enum_open_round_list
+        type(enum_open_round), public :: undefined
+            !! The enumerator to represent the `UNDEFINED`
+        type(enum_open_round), public :: up
+            !! The enumerator to represent the `UP`
+        type(enum_open_round), public :: down
+            !! The enumerator to represent the `DOWN`
+        type(enum_open_round), public :: zero
+            !! The enumerator to represent the `ZERO`
+        type(enum_open_round), public :: nearest
+            !! The enumerator to represent the `NEAREST`
+        type(enum_open_round), public :: compatible
+            !! The enumerator to represent the `COMPATIBLE`
+        type(enum_open_round), public :: processor_defined
+            !! The enumerator to represent the `PROCESSOR_DEFINED`
+    end type enum_open_round_list
+
+    type(enum_open_round_list), public, parameter :: &
+        open_round = &
+            enum_open_round_list( &
+                undefined         = enum_open_round(Open_Round_Undefined       , round(Open_Round_Undefined)), &
+                up                = enum_open_round(Open_Round_Up              , round(Open_Round_Up)), &
+                down              = enum_open_round(Open_Round_Down            , round(Open_Round_Down)), &
+                zero              = enum_open_round(Open_Round_Zero            , round(Open_Round_Zero)), &
+                nearest           = enum_open_round(Open_Round_Nearest         , round(Open_Round_Nearest)), &
+                compatible        = enum_open_round(Open_Round_Compatible      , round(Open_Round_Compatible)), &
+                processor_defined = enum_open_round(Open_Round_ProcessorDefined, round(Open_Round_ProcessorDefined)) &
+            ) !&
+        !! The enumerators of possible expressions
+        !! for the `round` specifier.
+
+    interface optval
+        procedure :: optval_open_round
+    end interface
+contains
+    !>Returns `x` if it is presented, and
+    !>returns `default` if the `x` is not presented.
+    function optval_open_round(x, default) result(y)
+        implicit none
+        type(enum_open_round), intent(in), optional :: x
+        type(enum_open_round), intent(in) :: default
+
+        type(enum_open_round) :: y
+
+        if (present(x)) then
+            y = x
+        else
+            y = default
+        end if
+    end function optval_open_round
+
+    !>Returns the enumerator representing the default
+    !>character-expression for the `round` specifier
+    !>in the `open` statement.
+    !>The default value is compiler-dependent.
+    !>
+    !>@warning
+    !>The default rounding mode was not obtained
+    !>if the rounding mode was changed before calling this procedure.
+    !>@endwarning
+    function get_open_round_default() result(default)
+        use, intrinsic :: ieee_arithmetic
+        use :: enumul_open_status
+        implicit none
+        type(enum_open_round) :: default
+            !! The enumerator for default value of `round` specifier
+            !! in `open`
+
+        type(ieee_round_type) :: rounding_mode
+
+        ! store the current rounding mode
+        call ieee_get_rounding_mode(rounding_mode)
+
+        if (rounding_mode == ieee_up) then
+            default = open_round%up
+            return
+        end if
+
+        if (rounding_mode == ieee_down) then
+            default = open_round%down
+            return
+        end if
+
+        if (rounding_mode == ieee_nearest) then
+            default = open_round%nearest
+            return
+        end if
+
+        if (rounding_mode == ieee_to_zero) then
+            default = open_round%zero
+            return
+        end if
+
+        default = open_round%processor_defined
+    end function get_open_round_default
+end module enumul_open_round
