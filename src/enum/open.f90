d9b09aa6f1346f6cb85b1fdbbac5ffd5d7403928
Tomohiro Degawa <degawa.tomohiro@gmail.com>
2023-09-04 11:00:24 +0900

    updated open module

--- src/enum/open.f90
+++ src/enum/open.f90
@@ -14,7 +14,7 @@ module enumul_open
     use :: enumul_open_encoding, &
         only:enum_open_encoding, open_encoding, default_open_encoding, get_open_encoding_default, optval
     use :: enumul_open_form, &
-        only:enum_open_form, open_form, default_open_form, get_open_form_default, optval
+        only:enum_open_form, open_form, get_open_form_default, optval
     use :: enumul_open_pad, &
         only:enum_open_pad, open_pad, default_open_pad, get_open_pad_default, optval
     use :: enumul_open_position, &
@@ -66,7 +66,7 @@ module enumul_open
     public :: get_open_decimal_default, default_open_decimal
     public :: get_open_delim_default, default_open_delim
     public :: get_open_encoding_default, default_open_encoding
-    public :: get_open_form_default, default_open_form
+    public :: get_open_form_default
     public :: get_open_pad_default, default_open_pad
     public :: get_open_position_default, default_open_position
     public :: get_open_round_default
